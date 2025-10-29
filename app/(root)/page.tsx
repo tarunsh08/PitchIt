c1d502ac01967614d869bff2a853ebccd3e9a066
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-10-29 19:07:06 +0530

    added author creation via github

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,15 @@ import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupCardType } from "../../components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
 
   const params = { search: query || null }
+
+  const session = await auth()
+  console.log(session?.id)
 
   const { data: posts } = await sanityFetch({query: STARTUPS_QUERY, params})
 
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -32,3 +32,7 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $
 export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
     _id, views
 }`)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == "author" && id == $id][0]{
+    _id, id, name, image, bio, username, email
+}`)
